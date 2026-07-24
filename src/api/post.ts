f2e0697e5dba79aa1af8ff4b2fd10db6d7abc157
reyjinn-IT <raihanpermadi17@gmail.com>
2026-07-24 11:33:44 +0700

    refactor: migrasi HTTP client dari fetch ke axios

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,19 +1,16 @@
+import { api } from "./axiosInstance";
+
 export type Post = {
     id: number;
     title: string;
     body: string;
 };
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com'
-
 export const fetchPosts = async (): Promise<Post[]> => {
-    const res = await fetch(`${BASE_URL}/posts?_limit=10`);
-    if (!res.ok) throw new Error('failed to fetch data posts')
-    return res.json()
-};
-
+    const res = await api.get<Post[]>(`/posts`, {params: {_limit : 10}})
+    return res.data;
+}
 export const fetchPostById = async (id: number): Promise<Post> => {
-    const res = await fetch(`${BASE_URL}/post/${id}`)
-    if (!res.ok) throw new Error("Post not found");
-    return res.json()
+    const res = await api.get<Post>(`/posts/${id}`)
+    return res.data;
 };
